fc74a5914bea61be8ea29baf8afc37ef476bdee0
olivia petrillo <opetrillo92@gmail.com>
2018-12-01 12:16:18 -0500

    some events

--- data/populate_events.sql
+++ data/populate_events.sql
@@ -48,13 +48,13 @@ insert into `event`(date, title, description, turnout) values
     show presented by Green Line Records featuring artists Margaret Glaspy and Told Slant. Hosted
      at Afterhours in the Curry Student Center at Northeastern University. Free show available to
       Northeastern students only. Doors open at 6:30.', 'Low'),
-  ('2018-11-29 17:30:00', 'WRBB & Green Line Records Present: Music in Context', 'WRBB and
+  ('2017-11-29 17:30:00', 'WRBB & Green Line Records Present: Music in Context', 'WRBB and
     Green Line Records present the first installment of Music in Context, a speaker series where
     we invite professionals involved in the music industry in various capacities to talk to
-    students about their careers. Join us November 29 as we host a discussion and Q&A with Greg
-    Shea, the Executive Producer of WGBH''s Front Row Boston, a 3-time New England Emmy Award
-    winning music performance series featuring exceptional artists across multiple genres from
-    Boston’s best music venues.', 'Low');
+    students about their careers. Join us November 29 in 224 Hurtig Hall as we host a discussion
+    and Q&A with Greg Shea, the Executive Producer of WGBH''s Front Row Boston, a 3-time New
+    England Emmy Award winning music performance series featuring exceptional artists across
+    multiple genres from Boston’s best music venues.', 'Low');
 
 insert into `event`(date, title, description) values
     ('2018-10-31 18:30:00', 'Edge Petal Burn, Daisybones, Nick Owen & Jack Kerwin', 'Live show
@@ -69,4 +69,109 @@ insert into `event`(date, title, description) values
     the Northeastern Songwriting Club’s EP. Hosted at Afterhours in the Curry Student Center at
     Northeastern University. Free show available to Northeastern students only. Doors open at
     6:00.');
+
+insert into `event`(date, title, description, turnout) values
+    ('2017-11-13 19:00:00', 'GLR Prsnts: The Pains of Being Pure at Heart, Ovlov, Tuxis Giant',
+     'Live show presented by Green Line Records featuring artists The Pains of Being Pure at
+     Heart, Ovlov, and Tuxis Giant. Hosted at Afterhours in the Curry Student Center at
+     Northeastern University. Free show available to Northeastern students and +1 only. Doors
+     open at 6:30.', 'Low'),
+    ('2017-10-19 11:00:00', 'Fall Busking Day 2017', 'Busking Day is an opportunity for
+    students to play musical sets before their peers. Presented by Green Line Records. Hosted at
+    the Freshman Quad between Speare Hall and Stetson East.', 'Low'),
+    ('2017-10-11 19:00:00', 'Green Line Records Presents: Ivy Sole and Pink Navel', 'Live show
+    presented by Green Line Records featuring artists Ivy Sole and Pink Navel. Hosted at
+    Afterhours in the Curry Student Center at Northeastern University. Free show available to
+    Northeastern students and +1 only. Doors open at 6:30.', 'Low'),
+    ('2017-04-17 19:00:00', 'Green Line Presents: Screaming Females, Gymshorts & Black Beach',
+     'Live show presented by Green Line Records featuring artists Screaming Females, Gymshorts,
+     and Black Beach. Hosted at Afterhours in the Curry Student Center at Northeastern University
+     . Free show available to Northeastern students and +1 only. Doors open at 6:30.', 'Medium'),
+    ('2017-04-12 19:00:00', 'LMA, GLR & CUP Present: Noname ft. Sammus + Akenya', 'Live show
+    presented by Live Music Association, Green Line Records, and CUP featuring artists  Noname
+    featuring Sammus and Akenya. Hosted at Afterhours in the Curry Student Center at Northeastern
+     University. Free show available to Northeastern students only. Doors open at 6:30.',
+     'High'),
+    ('2017-03-22 19:00:00', 'Green Line Records Presents: Tacocat, Big Eyes & Halfsour', 'Live
+    show presented by Green Line Records featuring artists Tacocat, Big Eyes, and Halfsour.
+    Hosted at Afterhours in the Curry Student Center at Northeastern University. Free show
+    available to Northeastern students and +1 only. Doors open at 6:30.', 'Medium'),
+    ('2017-02-13 19:00:00', 'Green Line Presents: PWR BTTM, Aye Nako, & DUMP HIM', 'Live show
+    presented by Green Line Records featuring artists PWR BTTM, Aye Nako, and DUMP HIM. Hosted at
+     Afterhours in the Curry Student Center at Northeastern University. Free show available to
+     Northeastern students only. Doors open at 6:30.', 'High'),
+    ('2017-01-23 19:00:00', 'Green Line Presents: Grouper, Japanese Breakfast (solo) & Bedbug',
+     'Live show presented by Green Line Records featuring artists Grouper, Japanese Breakfast
+     (solo), and Bedbug. Hosted at Afterhours in the Curry Student Center at Northeastern
+     University. Free show available to Northeastern students and +1 only. Doors open at 6:30.',
+     'High'),
+    ('2016-11-29 19:00:00', 'CUP and Green Line Present: Pinegrove/Mothers/Maggie Whitlock', 'Live
+     show presented by CUP and Green Line Records featuring artists Pinegrove, Mothers, and
+     Maggie Whitlock. Hosted at Afterhours in the Curry Student Center at Northeastern University
+     . Free show available to Northeastern students only. Doors open at 6:30.', 'High'),
+    ('2016-11-01 11:00:00', 'Fall Busking Day 2016', 'Busking Day is an opportunity for
+    students to play musical sets before their peers. Presented by Green Line Records. Hosted at
+    the Snell Library Quad.', 'Low'),
+    ('2016-10-30 19:00:00', 'HELL NIGHT JAMS ft. dust from 1000 yrs//Billy Baldwin//Bedbug',
+     'Live show presented by ONCE Ballroom and Green Line Records featuring artists dust from
+     1000 yrs, Billy Baldwin, and Bedbug. Hosted at ONCE Ballroom in Somerville, MA. Tickets cost
+      $14 online or $17 at the door.', 'Low'),
+    ('2016-10-17 19:00:00', 'Green Line Presents: Anamanaguchi, Crying & (T-T)b', 'Live show
+    presented by Green Line Records featuring artists Anamanaguchi, Crying, and (T-T)b. Hosted at
+     Afterhours in the Curry Student Center at Northeastern University. Free show available to
+     Northeastern students and +1 only. Doors open at 6:30.', 'High'),
+    ('2016-09-26 19:00:00', 'Green Line Records Presents: Turnover, All Dogs, & Daephne', 'Live
+    show presented by Green Line Records featuring artists Turnover, All Dogs, and Daephne.
+    Hosted at Afterhours in the Curry Student Center at Northeastern University. Free show
+    available to Northeastern students and +1 only. Doors open at 6:30.', 'High'),
+    ('2016-09-21 19:00:00', 'Green Line Presents: A Prince Tribute Show', 'Live show presented by
+     Green Line Records featuring student artists of Northeastern paying tribute to Prince.
+     Hosted at Afterhours in the Curry Student Center at Northeastern University. Free show
+     available to Northeastern students only. Doors open at 6:30.', 'Low'),
+    ('2016-04-20 16:00:00', 'Spring Busking Day 2016', 'Busking Day is an opportunity for
+    students to play musical sets before their peers. Presented by Green Line Records. Hosted at
+    the Snell Library Quad.', 'Medium'),
+    ('2016-04-13 19:00:00', 'Green Line Records Presents: Kilo Kish, Salt Cathedral, & Atlas
+    Lab', 'Live show presented by Green Line Records featuring artists Kilo Kish, Salt Cathedral,
+     and Atlas Lab. Hosted at Afterhours in the Curry Student Center at Northeastern University.
+     Free show available to Northeastern students and +1 only. Doors open at 6:30.', 'Medium'),
+    ('2016-03-24 19:00:00', 'Green Line Presents: Girlpool, Teen Suicide & Ricecrackers', 'Green
+    Line Presents: Live show presented by Green Line Records featuring artists Girlpool, Teen
+    Suicide, and Ricecrackers. Hosted at Afterhours in the Curry Student Center at Northeastern
+    University. Free show available to Northeastern students and +1 only. Doors open at 6:30.',
+     'High'),
+    ('2016-02-11 19:00:00', 'Green Line Presents: Porches, eskimeaux & Brittle Brian', 'Green
+    Line Presents:Live show presented by Green Line Records featuring artists Porches, eskimeaux,
+     and Brittle Brian. Hosted at Afterhours in the Curry Student Center at Northeastern
+     University. Free show available to Northeastern students and +1 only. Doors open at 6:30.',
+     'High'),
+    ('2016-02-06 19:00:00', 'Green Line Presents - David Bowie Tribute Night', 'Live show
+    presented by Green Line Records featuring student artists of Northeastern paying tribute to
+    David Bowie. Hosted at Afterhours in the Curry Student Center at Northeastern University.
+    Free show available to Northeastern students and +1 only. Doors open at 6:30.', 'Medium'),
+    ('2015-12-01 19:00:00', 'Green Line Presents...A Boston Showcase', 'Live show presented by
+    Green Line Records featuring artists Black Beach, Midriffs, NICE GUYS, and The TeleVibes.
+    Hosted at Afterhours in the Curry Student Center at Northeastern University. Free show
+    available to Northeastern students and +1 only. Doors open at 6:00.', 'Medium'),
+    ('2015-11-10 11:00:00', 'Fall Busking Day 2015', 'Busking Day is an opportunity for
+    students to play musical sets before their peers. Presented by Green Line Records. Hosted at
+    the Snell Library Quad.', 'Low'),
+    ('2015-11-09 19:00:00', 'Green Line Presents...Mitski, Cayetana & Littlefoot', 'Live show
+    presented by Green Line Records featuring artists Mitski, Cayetana, and Littlefoot. Hosted at
+     Afterhours in the Curry Student Center at Northeastern University. Free show available to
+     Northeastern students and +1 only. Doors open at 6:30.', 'High'),
+    ('2015-10-19 19:00:00', 'Green Line Presents...Together Pangea, Pile & Nice Guys', 'Live show
+     presented by Green Line Records featuring artists together PANGEA, Pile, and NICE GUYS.
+     Hosted at Afterhours in the Curry Student Center at Northeastern University. Free show
+     available to Northeastern students and +1 only. Doors open at 6:30.', 'High'),
+    ('2015-09-24 19:00:00', 'Green Line Records Presents...The Menzingers, Black Beach & Spectre
+    Shores', 'Live show presented by Green Line Records featuring artists The Menzingers, Black
+    Beach & Spectre Shores. Hosted at Afterhours in the Curry Student Center at Northeastern
+    University. Free show available to Northeastern students and +1 only. Doors open at 6:30.',
+     'High'),
+    ('2015-04-27 19:00:00', 'Green Line Records Presents: Mac DeMarco', 'Live show presented by
+    Green Line Records featuring artists Mac DeMarco (solo), Tall Juan, and Mackenzie Keefe.
+    Hosted at Afterhours in the Curry Student Center at Northeastern University. Free show
+    available to Northeastern students only. Doors open at 6:30.', 'High');
+
 
