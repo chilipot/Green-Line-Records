e4d8116520109fbeabb004bee153f2de3548376b
olivia petrillo <opetrillo92@gmail.com>
2018-12-07 18:05:28 -0500

    renamed the live session/recording jazz and updated diagram so it at least has all the tables and i hope info we need

--- data/populate_live_recording.sql
+++ data/populate_live_recording.sql
@@ -1,12 +1,12 @@
 USE green_line_records;
 
-DROP function if exists find_live_session_id;
+DROP function if exists find_live_recording_id;
 
 DELIMITER //
 
-DROP FUNCTION IF EXISTS find_live_session_id;
+DROP FUNCTION IF EXISTS find_live_recording_id;
 
-create function find_live_session_id
+create function find_live_recording_id
   (
     show_name_s  varchar(150),
     date_s       DATE,
@@ -14,8 +14,8 @@ create function find_live_session_id
   )
   returns int
   BEGIN
-    return (select live_session_id
-            from live_session
+    return (select live_recording_id
+            from live_recording
             where show_name like show_name_s
               and DATE(date) = date_s
               and TIME(start_time) = start_time_s
@@ -25,7 +25,7 @@ create function find_live_session_id
 DELIMITER ;
 
 
-INSERT INTO live_session (show_name, date, start_time, end_time, location_id)
+INSERT INTO live_recording (show_name, date, start_time, end_time, location_id)
 VALUES ('Solo Euphonium and NU Wind Ensemble', '2018-10-04', '19:20:00', '20:30:00', 2),
        ('Four Wind Ensembles (Bands of the Beanpot)', '2018-10-28', '15:00:00', '17:30:00', 2),
        ('Recital with Solo flute and Piano, and Solo Flute with NU Wind Ensemble',
